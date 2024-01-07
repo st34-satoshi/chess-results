d7d5e43df7c04af716a739a61dd5f30e534bb508
Satoshi Tanaka <st34.satoshi@gmail.com>
2024-01-07 12:08:42 +0900

    STは実際のデータを使う

--- app/models/player.rb
+++ app/models/player.rb
@@ -7,4 +7,46 @@ class Player < ApplicationRecord
   def to_param
     ncs_id
   end
+
+  def games
+    white_games + black_games
+  end
+
+  def games_of(time_type)
+    games.select {|game| game.time_type == time_type}
+  end
+
+  def win_games_of(time_type)
+    white_games.select {|game| game.time_type == time_type && game.white_point == 1} + black_games.select {|game| game.time_type == time_type && game.white_point == 0}
+  end
+
+  def loss_games_of(time_type)
+    white_games.select {|game| game.time_type == time_type && game.white_point == 0} + black_games.select {|game| game.time_type == time_type && game.white_point == 1}
+  end
+
+  def draw_games_of(time_type)
+    games.select {|game| game.time_type == time_type && game.white_point == 0.5}
+  end
+
+  def color_games_of(time_type, color)
+    color_games = color == "White" ? white_games : black_games
+    color_games.select {|game| game.time_type == time_type}
+  end
+
+  def color_win_games_of(time_type, color)
+    color_games = color == "White" ? white_games : black_games
+    p = color == "White" ? 1 : 0
+    color_games.select {|game| game.time_type == time_type && game.white_point == p}
+  end
+
+  def color_loss_games_of(time_type, color)
+    color_games = color == "White" ? white_games : black_games
+    p = color == "White" ? 0 : 1
+    color_games.select {|game| game.time_type == time_type && game.white_point == p}
+  end
+
+  def color_draw_games_of(time_type, color)
+    color_games = color == "White" ? white_games : black_games
+    color_games.select {|game| game.time_type == time_type && game.white_point == 0.5}
+  end
 end
