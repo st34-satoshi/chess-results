094306da4f4625e63b250c3b7becfa767af1964b
Satoshi Tanaka <st34.satoshi@gmail.com>
2024-01-07 11:37:57 +0900

    playerページの画面を作成

---
+++ app/javascript/player/show.js
@@ -0,0 +1,65 @@
+import "application"
+import "chart"
+import "chartjs-plugin-datalabels"
+
+// Register the plugin to all charts:
+Chart.register(ChartDataLabels);
+
+function drawCharts(){
+  const ctxAll = document.getElementById('stWholePeriodAllChart');
+  drawDoughnutChart(ctxAll, 'すべての対局')
+  const ctxWhite = document.getElementById('stWholePeriodWhiteChart');
+  drawDoughnutChart(ctxWhite, '白番の対局')
+  const ctxBlack = document.getElementById('stWholePeriodBlackChart');
+  drawDoughnutChart(ctxBlack, '黒番の対局')
+
+}
+
+function drawDoughnutChart(ctx, label){
+  /**
+   * canvasのdivを受け取って勝ち・引分・負けのdoughnut chartを描く
+   */
+
+  const data = {
+    labels: [
+      '勝ち',
+      '引分',
+      '負け'
+    ],
+    datasets: [{
+      label: label,
+      data: [ctx.dataset.win, ctx.dataset.draw, ctx.dataset.loss],
+      backgroundColor: [
+        'rgb(50,205,50)',
+        'rgb(169,169,169)',
+        'rgb(255, 99, 132)'
+      ],
+      hoverOffset: 4
+    }]
+  };
+
+  new Chart(ctx, {
+    type: 'doughnut',
+    data: data,
+    options: {
+      plugins: {
+        datalabels: {
+          formatter: (value, context) => {
+            let total = 0;
+            for(const v of context.dataset.data){
+              total += Number(v);
+            }
+            return `${value} (${Math.round(100*value/total)}%)`;
+          },
+          font: {
+            size: 18,
+          },
+        },
+      },
+    },
+  });
+}
+
+window.addEventListener("load", (event) => {
+  drawCharts();
+});
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,5 +4,8 @@
 
 pin 'application', preload: true
 pin 'bootstrap', preload: true
+pin 'chart', preload: true
+pin 'chartjs-plugin-datalabels', preload: true
+pin 'player/show', preload: true
 
 pin_all_from 'app/javascript/controllers', under: 'controllers'
