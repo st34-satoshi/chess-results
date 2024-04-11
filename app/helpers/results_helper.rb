5da993d4afa777c6d3dfe66363a81a56d06aeeba
Satoshi Tanaka <st34.satoshi@gmail.com>
2024-04-12 00:26:31 +0900

    URの表示

--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
 module ResultsHelper
-  def hello
-    "hello helper"
-  end
   def result_to_white_s(result)
     # floatの白の点数を受け取って 1-0, 1/2-1/2, 0-1に変換する
     return "1-0" if result == 1
     return "0-1" if result == 0
     return "1/2-1/2" if result == 0.5
     ""
+  end
+
+  def rating_format(rating)
+    return rating if rating.positive?
+    "UR"
   end
 end
