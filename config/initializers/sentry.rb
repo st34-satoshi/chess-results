a2f1b832f197e542c4568797310b33de16cc0843
Satoshi Tanaka <st34.satoshi@gmail.com>
2024-04-26 23:28:03 +0900

    sentry sample to 0.1

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -7,13 +7,9 @@ Sentry.init do |config|
   # Set traces_sample_rate to 1.0 to capture 100%
   # of transactions for performance monitoring.
   # We recommend adjusting this value in production.
-  config.traces_sample_rate = 1.0
-  # or
-  config.traces_sampler = lambda do |_context|
-    true
-  end
+  config.traces_sample_rate = 0.1
   # Set profiles_sample_rate to profile 100%
   # of sampled transactions.
   # We recommend adjusting this value in production.
-  config.profiles_sample_rate = 1.0
+  config.profiles_sample_rate = 0.1
 end
